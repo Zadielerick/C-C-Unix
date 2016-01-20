45d3e8a810cc019e24d804680773539be7d581bc
Erick Narvaez <zadielerick@gmail.com>
2016-01-20 13:16:39 -0800

    Comment and Improve
    Added new line after Hello World.

--- Pgm1/HelloWorld.cpp
+++ Pgm1/HelloWorld.cpp
@@ -1,7 +1,15 @@
 //HelloWorld.cpp
 //Author: Erick Narvaez
+//Classes: (none)
+//Procedures:
+//  int main() : Output "Hello World".
+
 #include <iostream>
 
+// Name: main
+// Author: Erick Narvaez
+// Summary: Output "Hello World" using iostream
+// Arguments: (none)
 int main() {
-  std::cout << "Hello World!";
+  std::cout << "Hello World!\n";
 }
